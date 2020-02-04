aadb657b9f19e0dbb07dc9431ac065d6105f4f45
dsarghe@gmail.com <dsarghe@gmail.com>
2020-02-04 17:56:20 +0200

    First commit

---
+++ ShoeStoreFinal/DataModel/Migrations/Configuration.cs
@@ -0,0 +1,76 @@
+﻿namespace DataModel.Migrations
+{
+    using ModelsClasses;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DataModel.StoreContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DataModel.StoreContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+
+            var brand = new Brand
+            {
+                Brandid = 1,
+                Name = "Adidas"
+
+            };
+
+            context.Brands.AddOrUpdate(x => x.Brandid, brand);
+
+            var order = new Order
+            {
+                Orderid = 1,
+                UserName = "gsfgfr",
+                FirstName = "gigi",
+                LastName = "kent",
+                Adress = "Str.Mare",
+                City = "Iasi",
+                PostalCode = "70250",
+                Country = "Romania",
+                Email = "dhuhr@gmail.com",
+                Phone = "0700000000",
+                Total = 250,
+                OrderDate = DateTime.Now
+               
+                
+            };
+            context.Orders.AddOrUpdate(x => x.Orderid, order);
+
+            var product = new Product
+            {
+                Productid = 1,
+                Genreid = 3,
+                Brandid = 4,
+                Title = "Casual",
+                Price = 69,
+                Size = 37
+
+
+            };
+            context.Products.AddOrUpdate(x => x.Productid, product);
+
+            var orderDetails = new OrderDetail
+            {
+                OrderDetailid = 1,
+                Orderid = 2,
+                Productid = 1,
+                Quantity = 2,
+                UnitPrice = 56
+            };
+            context.OrderDetails.AddOrUpdate(x => x.OrderDetailid, orderDetails);
+        }
+    }
+}
---
+++ ShoeStoreFinal/ModelsClasses/Models.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelsClasses
+{
+    [Table("Brand")]
+    public class Brand
+    {
+        //Brand brand = new Brand();
+        [Key]
+        [Column(Order = 0)]
+        public int Brandid { get; set; }
+        [Column(Order = 1)]
+        public string Name { get; set; }
+
+    }
+
+    [Table("Genre")]
+    public class Genre
+    {
+        [Key]
+        [Column("GenreId", Order = 0)]
+        public int Genreid { get; set; }
+        [Column(Order = 1)]
+        public string Name { get; set; }
+
+        [Column(Order = 2)]
+        public string Description { get; set; }
+        public List<Product> Products { get; set; }
+
+    }
+
+    [Table("Product")]
+    public class Product
+    {
+        
+        [Key]
+        [Column(Order = 0)]
+        public int Productid { get; set; }
+        [Column(Order = 1)]
+        public int Genreid { get; set; }
+        [Column(Order = 2)]
+        public int Brandid { get; set; }
+        [Column(Order = 3)]
+        public string Title { get; set; }
+        [Column(Order = 4)]
+        public decimal Price { get; set; }
+        [Column(Order = 5)]
+        public int Size { get; set; }
+        public virtual Genre Genre { get; set; }
+        public virtual Brand Brand { get; set; }
+
+    }
+
+    [Table("Order")]
+    public class Order
+    {
+        [Key]
+        [Column(Order = 0)]
+        public int Orderid { get; set; }
+        [Column(Order = 12)]
+        public DateTime OrderDate { get; set; }
+        [Column(Order = 1)]
+        public string UserName { get; set; }
+        [Column(Order = 2)]
+        public string FirstName { get; set; }
+        [Column(Order = 3)]
+        public string LastName { get; set; }
+        [Column(Order = 4)]
+        public string Adress { get; set; }
+        [Column(Order = 5)]
+        public string City { get; set; }
+        [Column(Order = 6)]
+        public string PostalCode { get; set; }
+        [Column(Order = 7)]
+        public string Country { get; set; }
+        [Column(Order = 8)]
+        public string Email { get; set; }
+        [Column(Order = 9)]
+        public string Phone { get; set; }
+        [Column(Order = 10)]
+        public decimal Total { get; set; }
+        [Column(Order = 11)]
+        public List<OrderDetail> OrderDetails { get; set; }
+
+    }
+
+    [Table("OrderDetail")]
+
+    public class OrderDetail
+    {
+        [Key]
+        [Column(Order = 0)]
+        public int OrderDetailid { get; set; }
+        [Column(Order = 1)]
+        public int Orderid { get; set; }
+        [Column(Order = 2)]
+        public int Productid { get; set; }
+        [Column(Order = 3)]
+        public int Quantity { get; set; }
+        [Column(Order = 4)]
+        public decimal UnitPrice { get; set; }
+        [Column(Order = 5)]
+        public virtual Product Product { get; set; }
+        public virtual Order Order { get; set; }
+
+    }
+    [Table("Cart")]
+
+    public class Cart
+    {
+        [Key]
+        [Column(Order = 0)]
+        public int Recordid { get; set; }
+        [Column(Order = 1)]
+        public int CartId { get; set; }
+        [Column(Order = 2)]
+        public int ProductId { get; set; }
+        [Column(Order = 3)]
+        public int Count { get; set; }
+        [Column(Order = 4)]
+        public virtual Product Product { get; set; }
+    }
+
+}
